30e874bfa8cd6aab0d9103e4206e7e891d4c00d4
mohamedgonl <82462231+mohamedgonl@users.noreply.github.com>
2021-12-03 16:50:30 +0700

    Create 26.cpp

---
+++ 26.cpp
@@ -0,0 +1,44 @@
+Given integers n, l and r, find the number of ways to represent n as a sum of two integers A and B such that l ≤ A ≤ B ≤ r.
+
+Example
+
+For n = 6, l = 2, and r = 4, the output should be
+solution(n, l, r) = 2.
+
+There are just two ways to write 6 as A + B, where 2 ≤ A ≤ B ≤ 4: 6 = 2 + 4 and 6 = 3 + 3.
+
+Input/Output
+
+[execution time limit] 0.5 seconds (cpp)
+
+[input] integer n
+
+A positive integer.
+
+Guaranteed constraints:
+5 ≤ n ≤ 109.
+
+[input] integer l
+
+A positive integer.
+
+Guaranteed constraints:
+1 ≤ l ≤ r.
+
+[input] integer r
+
+A positive integer.
+
+Guaranteed constraints:
+l ≤ r ≤ 109,
+r - l ≤ 106.
+
+[output] integer
+-------------------------------------------------------------------
+int solution(int n, int l, int r) {
+    int count =0;
+    for(int i=l;i<=r;i++){
+        if((n-i<=r)&&(n-i>=i)) count++;   
+    }
+    return count;
+}
